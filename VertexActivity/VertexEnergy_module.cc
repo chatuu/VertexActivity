c3c94907d1786f67c93cf8370c3e278b784efdcf
Chatura Kuruppu <ckuruppu@email.sc.edu>
2020-09-17 22:22:54 -0500

    Volumes, labels were added to the fcl file as fcl parameters.

--- VertexActivity/VertexEnergy_module.cc
+++ VertexActivity/VertexEnergy_module.cc
@@ -6,140 +6,7 @@
 // Generated at Wed Sep 16 23:24:57 2020 by Chatura Kuruppu using cetskelgen
 // from cetlib version v3_06_01.
 ////////////////////////////////////////////////////////////////////////
-
-#include "CAFMaker/CAFMakerParams.h"
-#include "CAFMaker/FillEnergies.h"
-#include "CAFMaker/FillPIDs.h"
-#include "CAFMaker/FillParentInfo.h"
-#include "CAFMaker/FillReco.h"
-#include "CAFMaker/FillTruth.h"
-#include "CAFMaker/Utils.h"
-// C/C++ includes
-#include <fenv.h>
-#include <time.h>
-#include <algorithm>
-#include <cmath>
-#include <iomanip>
-#include <iostream>
-#include <iterator>
-#include <map>
-#include <sstream>
-#include <string>
-#include <vector>
-#ifdef DARWINBUILD
-#include <libgen.h>
-#endif
-// ROOT includes
-#include "TFile.h"
-#include "TH1D.h"
-#include "TTree.h"
-// Framework includes
-#include <art/Framework/Services/Optional/TFileService.h>
-#include <art/Framework/Services/Optional/TFileDirectory.h>
-#include "art/Framework/Core/EDProducer.h"
-#include "art/Framework/Core/FileBlock.h"
-#include "art/Framework/Core/ModuleMacros.h"
-#include "art/Framework/Principal/Event.h"
-#include "art/Framework/Principal/Handle.h"
-#include "art/Framework/Principal/SubRun.h"
-#include "art/Framework/Services/Optional/TFileDirectory.h"
-#include "art/Framework/Services/Registry/ServiceHandle.h"
-#include "canvas/Persistency/Common/FindMany.h"
-#include "canvas/Persistency/Common/FindManyP.h"
-#include "canvas/Persistency/Common/FindOne.h"
-#include "canvas/Persistency/Common/FindOneP.h"
-#include "canvas/Persistency/Common/Ptr.h"
-#include "canvas/Persistency/Common/PtrVector.h"
-#include "cetlib_except/exception.h"
-#include "fhiclcpp/ParameterSet.h"
-#include "messagefacility/MessageLogger/MessageLogger.h"
-//#include <IFDH_service.h>
-// NOvASoft includes
-// Geo/Cal/Util
-#include "Calibrator/Calibrator.h"
-#include "ChannelInfo/BadChanList.h"
-#include "Geometry/Geometry.h"
-#include "Geometry/LiveGeometry.h"
-#include "GeometryObjects/CellGeo.h"
-#include "GeometryObjects/Geo.h"
-#include "GeometryObjects/PlaneGeo.h"
-#include "Metadata/MetadataManager.h"
-#include "NovaDAQConventions/DAQConventions.h"
-#include "RawData/RawTrigger.h"
-#include "RunHistory/service/RunHistoryService.h"
-#include "Utilities/func/MathUtil.h"
-// DataQuality
-#include "DataQuality/DAQHeader/DAQEventSummary.h"
-// Sim/BackTracker
-#include "MCCheater/BackTracker.h"
-#include "MCReweight/FluxWeights.h"
-#include "Simulation/GENIEReweightTable.h"
-#include "nusimdata/SimulationBase/GTruth.h"
-#include "nusimdata/SimulationBase/MCFlux.h"
-#include "nusimdata/SimulationBase/MCNeutrino.h"
-#include "nusimdata/SimulationBase/MCParticle.h"
-#include "nusimdata/SimulationBase/MCTruth.h"
-// RecoBase
-#include "RecoBase/CellHit.h"
-#include "RecoBase/Cluster.h"
-#include "RecoBase/Energy.h"
-#include "RecoBase/FilterList.h"
-#include "RecoBase/FitSum.h"
-#include "RecoBase/Prong.h"
-#include "RecoBase/RecoHit.h"
-#include "RecoBase/Shower.h"
-#include "RecoBase/Track.h"
-#include "RecoBase/Vertex.h"
-#include "RecoBase/WeightedProng.h"
-// Other Reco
-#include "CVN/func/CVNFeatures.h"
-#include "CVN/func/PixelMap.h"
-#include "CVN/func/ProngTrainingData.h"
-#include "CVN/func/Result.h"
-#include "CVN/func/RegResult.h"
-#include "CVN/func/TrainingData.h"
-#include "CosRej/CosRejObj.h"
-#include "CosRej/NueCosRej.h"
-#include "CosRej/TrkCntObj.h"
-#include "LEM/PIDDetails.h"
-#include "LSTME/prediction/LSTMEnergy.h"
-#include "MEFinder/MEClusters.h"
-#include "MuonRemove/art/MRCCParent.h"
-#include "NCID/NCCosRej.h"
-#include "NumuEnergy/NumuE.h"
-#include "Preselection/PreselObj.h"
-#include "Preselection/Veto.h"
-#include "ReMId/ReMId.h"
-#include "RecVarPID/RVP.h"
-#include "RecoJMShower/EID.h"
-#include "RecoJMShower/JMShower.h"
-#include "ShowerLID/DedxDistribution.h"
-#include "ShowerLID/EventLID.h"
-#include "ShowerLID/ShowerLID.h"
-#include "ShowerLID/ShowerPID.h"
-#include "ShowerLID/SliceLID.h"
-#include "SliceLID/prediction/Prediction.h"
-#include "XSecReco/art/NCPi0BkgRej.h"
-#include "XnuePID/Xnue.h"
-#include "TensorFlowProducts/CVNCosmicFiltList.h"
-#include "TrackInfo/TrackInfoObj.h"
-// SummData
-#include "SummaryData/CosmicExposure.h"
-#include "SummaryData/EventQuality.h"
-#include "SummaryData/POTSum.h"
-#include "SummaryData/SpillData.h"
-// OscLib - for woscdumb
-//#include "OscLib/func/OscCalculatorDumb.h"
-// StandardRecord
-#include "StandardRecord/SRSpillTruthBranch.h"
-#include "StandardRecord/StandardRecord.h"
-// CAFMaker support
-#include "CAFMaker/Blinding.h"
-#include "Utilities/AssociationUtil.h"
-
-// Custom made headers
-#include "structs.h"
-
+#include "headers.h"
 class VertexEnergy;
 
 class VertexEnergy : public art::EDProducer
@@ -169,26 +36,74 @@ public:
   void respondToCloseOutputFiles(art::FileBlock const &fb) override;
   void respondToOpenInputFile(art::FileBlock const &fb) override;
   void respondToOpenOutputFiles(art::FileBlock const &fb) override;
+  void reconfigure(const fhicl::ParameterSet &pset);
 
 private:
   // Declare member data here.
+  template <class T>
+  void GetByLabelStrict(const art::Event &evt, const std::string &label,
+                        art::Handle<T> &handle) const;
+
+  template <class T, class U>
+  art::FindManyP<T> FindManyPStrict(const U &from, const art::Event &evt,
+                                    const art::InputTag &label) const;
+  std::string fSlicerLabel;
+  std::string fElasticArmsLabel;
+  std::string fCellHitLabel;
+
+  fhicl::ParameterSet fVol1, fVol2, fVol3, fVol4;
+  std::vector<fhicl::ParameterSet> fVols;
+  //std::array test;
+
+  std::vector<volume> vols;
 };
 
 VertexEnergy::VertexEnergy(fhicl::ParameterSet const &p)
 // :
 // Initialize member data here.
 {
-  // Call appropriate produces<>() functions here.
+  fSlicerLabel = p.get<std::string>("SlicerLabel");
+  fElasticArmsLabel = p.get<std::string>("ElasticArmsLabel");
+  fCellHitLabel = p.get<std::string>("CellHitLabel");
+
+  fVols.push_back(p.get<fhicl::ParameterSet>("volume1"));
+  fVols.push_back(p.get<fhicl::ParameterSet>("volume2"));
+  fVols.push_back(p.get<fhicl::ParameterSet>("volume3"));
+  fVols.push_back(p.get<fhicl::ParameterSet>("volume4"));
 }
 
 void VertexEnergy::produce(art::Event &e)
 {
   // Implementation of required member function here.
+  art::ServiceHandle<cheat::BackTracker> bt;
+  art::Handle<std::vector<rb::Cluster>> slices;
+  GetByLabelStrict(e, fSlicerLabel, slices);
+  auto fmElastic = FindManyPStrict<rb::Vertex>(slices, e, fElasticArmsLabel);
+  art::Handle<std::vector<rb::CellHit>> htHandle;
+  e.getByLabel(fCellHitLabel, htHandle);
+  art::PtrVector<rb::CellHit> allHits;
 }
 
 void VertexEnergy::beginJob()
 {
   // Implementation of optional member function here.
+  printf("\nVertex Activity Module Started..!\n");
+  //std::cout << "testing: " << fVol1.get<int>("xMin") << "\n";
+  printf("\nVolumes defined:");
+  unsigned int vol = 1;
+  for (auto it = fVols.begin(); it != fVols.end(); ++it)
+  {
+    printf("\nVolume %d: Xmin: %d Xmax: %d Ymin: %d Ymax: %d Zmin: %d Zmax: %d",
+           vol,
+           it->get<int>("xMin"),
+           it->get<int>("xMax"),
+           it->get<int>("yMin"),
+           it->get<int>("yMax"),
+           it->get<int>("zMin"),
+           it->get<int>("zMax"));
+    vol++;
+  }
+  printf("\n\n\n\n");
 }
 
 void VertexEnergy::beginRun(art::Run &r)
@@ -204,6 +119,7 @@ void VertexEnergy::beginSubRun(art::SubRun &sr)
 void VertexEnergy::endJob()
 {
   // Implementation of optional member function here.
+  printf("\nVertex Activity Module has finished job..!\n");
 }
 
 void VertexEnergy::endRun(art::Run &r)
@@ -235,5 +151,38 @@ void VertexEnergy::respondToOpenOutputFiles(art::FileBlock const &fb)
 {
   // Implementation of optional member function here.
 }
+//......................................................................
+template <class T>
+void VertexEnergy::GetByLabelStrict(const art::Event &evt,
+                                    const std::string &label,
+                                    art::Handle<T> &handle) const
+{
+  evt.getByLabel(label, handle);
+  if (!label.empty() && handle.failedToGet())
+  {
+    std::cout << "CAFMaker: No product of type '"
+              << abi::__cxa_demangle(typeid(*handle).name(), 0, 0, 0) << "' found under label '" << label << "'. "
+              << "Set 'StrictMode: false' to continue anyway." << std::endl;
+    abort();
+  }
+}
+//......................................................................
+template <class T, class U>
+art::FindManyP<T> VertexEnergy::FindManyPStrict(const U &from,
+                                                const art::Event &evt,
+                                                const art::InputTag &tag) const
+{
+  art::FindManyP<T> ret(from, evt, tag);
+  if (!tag.label().empty() && !ret.isValid())
+  {
+    std::cout << "CAFMaker: No Assn from '" << abi::__cxa_demangle(typeid(from).name(), 0, 0, 0) << "' to '"
+              << abi::__cxa_demangle(typeid(T).name(), 0, 0, 0)
+              << "' found under label '" << tag << "'. "
+              << "Set 'StrictMode: false' to continue anyway." << std::endl;
+    abort();
+  }
+  return ret;
+}
+//......................................................................
 
 DEFINE_ART_MODULE(VertexEnergy)
